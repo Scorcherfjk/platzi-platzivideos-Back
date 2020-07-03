1a73d4aac71334e2ac64c1560cf22f98396e9f7f
Fjavier De Freitas <franjavikale@gmail.com>
2020-07-03 18:57:37 -0500

    middleware errorhandler

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const app = express();
 
 const { port } = require('./config');
 const moviesApi = require('./routes/movies');
+const { logErrors, errorHandler } = require('./middlewares/errorHandlers');
+
+app.use(express.json());
 
 moviesApi(app);
 
+app.use(logErrors);
+app.use(errorHandler);
+
 app.listen(port, (err) => {
-  if (err) console.log(err.message);
+  if (err) console.error(err.message);
   console.log(`listening on http://localhost:${port}`)
 });
