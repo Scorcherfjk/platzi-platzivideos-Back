675a6876e27a0a2e1aec8aa9ab146b0d653c9696
Fjavier De Freitas <franjavikale@gmail.com>
2020-07-03 18:57:15 -0500

    config mongo conection

--- config/index.js
+++ config/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 const config = {
-  dev: process.env.NODE_ENV !== 'production',
+  dev: process.env.NODE_ENV.trim() !== 'production',
   port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  dbHost: process.env.DB_HOST,
 }
 
 module.exports = config;
